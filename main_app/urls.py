1e4d1634dbd28e4fd4722e8e4e432e3ce64d4ffc
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-11-27 13:02:56 -0500

    init commit

---
+++ main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+from main_app.views import HomeView
+
+urlpatterns = [
+
+]
---
+++ main_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render,redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView,LoginView
+
+# Create your views here.
