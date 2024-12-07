a1f6f63d7bbd3fac8c7f6bd3577aa7c13fa7786f
xsm3z <xsm.zhang@gmail.com>
2024-12-06 22:25:00 -0500

    Create plant, plot and garden forms updated

---
+++ main_app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Garden, Plot, Plant
+
+class GardenForm(forms.ModelForm):
+    class Meta:
+        model = Garden
+        fields = ['name', 'location', 'user']
+
+
+class PlotForm(forms.ModelForm):
+    class Meta:
+        model = Plot
+        fields = ['name', 'garden', 'dayssincewatered']
+
+
+class PlantForm(forms.ModelForm):
+    class Meta:
+        model = Plant
+        fields = ['name', 'dayssinceplanted', 'daysuntilmature', 'description', 'plot']
